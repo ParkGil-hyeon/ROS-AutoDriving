da244e2eb0edc1701dfd17927acccf5b9b80947d
ParkGil-hyeon <54922657+ParkGil-hyeon@users.noreply.github.com>
2019-11-26 15:20:01 +0900

    Update laneDetection.py

--- laneDetection.py
+++ laneDetection.py
@@ -2,65 +2,78 @@
 
 import cv2, time
 import numpy as np
-import math
 
 cap = cv2.VideoCapture('2.avi')
 
-value_threshold = 190
-
 image_width = 640
-scan_width, scan_height = 200, 100
-lmid, rmid = scan_width, image_width - scan_width
-area_width, area_height = 20, 10
-roi_vertical_pos = 280
-row_begin = (scan_height - area_height) // 2
-row_end = row_begin + area_height
-pixel_cnt_threshold = 0.35 * area_width * area_height
+scan_height = 80
+roi_vertical_pos = 270
 
 while True:
-   ret, frame = cap.read()
-   if not ret:
-       break
-   if cv2.waitKey(1) & 0xFF == 27:
-       break
+    ret, frame = cap.read()
+    if not ret:
+        break
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
 
-   roi = frame[roi_vertical_pos:roi_vertical_pos + scan_height, :]
-   # roi = frame
-   hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-   blur = cv2.GaussianBlur(hsv, (5, 5), 0)
+    roi = frame[roi_vertical_pos:roi_vertical_pos + scan_height, :]
+    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    roi = cv2.GaussianBlur(roi, (5, 5), 0)
+    roi = cv2.Canny(roi, 50, 100)
+    lines = cv2.HoughLines(roi, 1, np.pi/180, 80)
 
-   lbound = np.array([0, 0, value_threshold], dtype=np.uint8)
-   ubound = np.array([131, 255, 255], dtype=np.uint8)
-   dst = cv2.Canny(blur, 50, 200, None, 3)
+    semi = np.zeros(shape=(scan_height, image_width, 3), dtype=np.uint8)
+    result = np.zeros(shape=(scan_height, image_width, 3), dtype=np.uint8)
+    left = [0, 0, 0, 0]
+    left_count = 0
+    right = [0, 0, 0, 0]
+    right_count = 0
+    if lines is not None:
+        for line in lines:
+            for rho, theta in line:
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
 
-   cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
-   cdstP = np.copy(cdst)
+                p1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+                p2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+                cv2.line(semi, p1, p2, (0, 0, 255), 3, cv2.LINE_AA)
 
-   lines = cv2.HoughLines(dst, 1, np.pi / 180, 15, None, 0, 0)
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * (a))
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * (a))
 
-   if lines is not None:
-       for i in range(0, len(lines)):
-           rho = lines[i][0][0]
-           theta = lines[i][0][1]
-           a = math.cos(theta)
-           b = math.sin(theta)
-           x0 = a * rho
-           y0 = b * rho
-           pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
-           pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
-           cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+                if (y2 - y1)/(x2 - x1) <= -0.2:
+                    left[0] += x1
+                    left[1] += y1
+                    left[2] += x2
+                    left[3] += y2
+                    left_count += 1
+                elif (y2 - y1)/(x2 - x1) >= 0.2:
+                    right[0] += x1
+                    right[1] += y1
+                    right[2] += x2
+                    right[3] += y2
+                    right_count += 1
 
-   linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
+    if left_count > 0:
+        for i, value in enumerate(left):
+            left[i] = value//left_count
+    if right_count > 0:
+        for i, value in enumerate(right):
+            right[i] = value//right_count
 
-   if linesP is not None:
-       for i in range(0, len(linesP)):
-           l = linesP[i][0]
-           cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
-   cv2.imshow('test', cdstP)
+    cv2.line(result, (left[0], left[1]), (left[2], left[3]), (255, 0, 0), 3, cv2.LINE_AA)
+    cv2.line(result, (right[0], right[1]), (right[2], right[3]), (255, 0, 0), 3, cv2.LINE_AA)
 
-   cv2.imshow("origin", frame)
+    cv2.imshow('origin', frame)
+    cv2.imshow('canny', roi)
+    cv2.imshow('semi', semi)
+    cv2.imshow('result', result)
 
-   time.sleep(0.01)
+    time.sleep(0.005)
 
 cap.release()
 cv2.destroyAllWindows()
